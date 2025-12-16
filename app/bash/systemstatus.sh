ddfb1a3d48caecd7dc6e5bde02bc660788f2423e
jvanderzande <jvdzande@yahoo.com>
2025-12-16 20:28:44 +0100

    update systemstatus.sh

--- app/bash/systemstatus.sh
+++ app/bash/systemstatus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ####################################################################################
-# xxxChange the rid values below to match the sensors on your devices page in Domoticz
+# Change the rid values below to match the sensors on your devices page in Domoticz
 ####################################################################################
 #Get Revision to be able to determine the API format to use
 CheckforUpdate=`curl $DomoticzURL'/json.htm?type=command&param=checkforupdate&forced=true' 2>/dev/null`
@@ -19,20 +19,21 @@ fi
 ##############################################################################
 ResultString="-CPU temperature: "
 
-ResultString+=`curl $DomoticzURL'/json.htm?type='$Apichange'devices&rid=40' 2>/dev/null | jq -r .result[]."Temp"`
+# ResultString+=`curl $DomoticzURL'/json.htm?type='$Apichange'devices&rid=40' 2>/dev/null | jq -r '.result[].Data'`
+# - or -
+ResultString+=`curl $DomoticzURL'/json.htm?type='$Apichange'devices&rid=40' 2>/dev/null | jq -r '.result[].Temp | (. * 10 | round) / 10'`
 ResultString+="°C\n"
-#~ ResultString+="C\n"
 
 ResultString+="-CPU Usage: "
-ResultString+=`curl $DomoticzURL'/json.htm?type='$Apichange'devices&rid=56' 2>/dev/null | jq -r .result[]."Data"`
+ResultString+=`curl $DomoticzURL'/json.htm?type='$Apichange'devices&rid=56' 2>/dev/null | jq -r '.result[].Data'`
 ResultString+="\n"
 
 ResultString+="-Memory Usage: "
-ResultString+=`curl $DomoticzURL'/json.htm?type='$Apichange'devices&rid=53' 2>/dev/null | jq -r .result[]."Data"`
+ResultString+=`curl $DomoticzURL'/json.htm?type='$Apichange'devices&rid=53' 2>/dev/null | jq -r '.result[].Data'`
 ResultString+="\n"
 
 ResultString+="-SD usage: "
-ResultString+=`curl $DomoticzURL'/json.htm?type='$Apichange'devices&rid=55' 2>/dev/null | jq -r .result[]."Data"`
+ResultString+=`curl $DomoticzURL'/json.htm?type='$Apichange'devices&rid=55' 2>/dev/null | jq -r '.result[].Data`
 ResultString+="\n"
 
 if [[ "$HaveUpdate" -eq "true" ]] ; then
